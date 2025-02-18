7884daf13598c55f59202e2ebf501f7ad463b25a
Adrián Ortega Rodríguez <adrian>
2025-02-18 09:39:24 +0100

    read linkedin zip

---
+++ electron/api/linkedin.mjs
@@ -0,0 +1,34 @@
+//const extract = require('extract-zip');
+import extract from 'extract-zip';
+import fs from 'fs';
+import path from 'path';
+import Papa from 'papaparse';
+
+export const getUserFromLinkedinZip = async (zipPath) => {
+  try {
+    const targetPath = path.resolve('db/linkedin');
+    const response = await extract(zipPath, { dir: targetPath });
+    const documents = ['Profile', 'Positions', 'Skills'];
+    const data = [];
+    for (const document of documents) {
+      const filePath = path.resolve(targetPath, `${document}.csv`);
+      if (!fs.existsSync(filePath)) continue;
+
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const parsedCsv = Papa.parse(fileContent, {
+        header: true,
+        skipEmptyLines: true,
+      });
+
+      data[document] = parsedCsv.data;
+    }
+    return data;
+  } catch (error) {
+    console.log('Error getting user from LinkedIn zip', error);
+  }
+};
+
+const test = async () => {
+  await getUserFromLinkedinZip('/Users/pc/Downloads/linkedin.zip');
+};
+test();
