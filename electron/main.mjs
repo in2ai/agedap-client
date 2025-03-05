b64d831dd1285cfb2fe7d88595dec536bbe5da78
Adrian <adri1pawn@gmail.com>
2025-03-05 12:18:44 +0100

    run in bg

--- electron/main.mjs
+++ electron/main.mjs
@@ -5,10 +5,11 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu, Tray } from 'electron';
 import { handleRunNodeCode } from './api/nodeFunctions.mjs';
 
 let mainWindow;
+let tray;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -24,8 +25,34 @@ function createWindow() {
   mainWindow.loadFile(path.join(__dirname, '../dist/agedap-llamatron/browser/index.html'));
 
   mainWindow.webContents.openDevTools();
+
+  tray = new Tray('public/icon.png');
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Abrir', click: () => mainWindow.show() },
+    {
+      label: 'Salir',
+      click: () => {
+        mainWindow.destroy();
+        app.quit();
+      },
+    },
+  ]);
+  tray.setContextMenu(contextMenu);
+
+  tray.setToolTip('Llamatron');
+  tray.on('click', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('close', (event) => {
+    event.preventDefault(); // Evita el cierre de la app
+    mainWindow.hide(); // Oculta la ventana en lugar de cerrarla
+  });
 }
 
 app.whenReady().then(createWindow);
+app.on('window-all-closed', (event) => {
+  event.preventDefault(); // Evita que la app se cierre completamente
+});
 
 handleRunNodeCode(); // Llama a las funciones del backend
